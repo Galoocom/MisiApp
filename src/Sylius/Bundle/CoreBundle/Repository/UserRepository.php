5b4b83226294a30df9dadf3a6052015b795e2427
pentarim <pentarim@gmail.com>
2013-09-11 12:19:08 +0200

    creating misi userprovider

--- src/Sylius/Bundle/CoreBundle/Repository/UserRepository.php
+++ src/Sylius/Bundle/CoreBundle/Repository/UserRepository.php
@@ -12,18 +12,14 @@
 namespace Sylius\Bundle\CoreBundle\Repository;
 
 use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
-use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
-use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
-use Doctrine\ORM\NoResultException;
 
 /**
  * User repository.
  *
  * @author Saša Stamenković <umpirsky@gmail.com>
  */
-class UserRepository extends EntityRepository implements UserProviderInterface
+class UserRepository extends EntityRepository
 {
     /**
      * Create filter paginator.
@@ -64,49 +60,4 @@ class UserRepository extends EntityRepository implements UserProviderInterface
 
         return $this->getPaginator($queryBuilder);
     }
-    
-    public function loadUserByUsername($username)
-    {
-        $q = $this
-            ->createQueryBuilder('u')
-            ->where('u.username = :username OR u.email = :email')
-            ->setParameter('username', $username)
-            ->setParameter('email', $username)
-            ->getQuery();
-
-        try {
-            // The Query::getSingleResult() method throws an exception
-            // if there is no record matching the criteria.
-            $user = $q->getSingleResult();
-        } catch (NoResultException $e) {
-            $message = sprintf(
-                'Unable to find an active admin AcmeUserBundle:User object identified by "%s".',
-                $username
-            );
-            throw new UsernameNotFoundException($message, 0, $e);
-        }
-
-        return $user;
-    }
-
-    public function refreshUser(UserInterface $user)
-    {
-        $class = get_class($user);
-        if (!$this->supportsClass($class)) {
-            throw new UnsupportedUserException(
-                sprintf(
-                    'Instances of "%s" are not supported.',
-                    $class
-                )
-            );
-        }
-
-        return $this->find($user->getId());
-    }
-
-    public function supportsClass($class)
-    {
-        return $this->getEntityName() === $class
-            || is_subclass_of($class, $this->getEntityName());
-    }    
 }
