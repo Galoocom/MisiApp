c4018d283c2dd3e4c0a8820be2f922d019a7f10c
pentarim <pentarim@gmail.com>
2013-10-07 10:33:44 +0200

    remove origin check for dev

--- web/app_dev.php
+++ web/app_dev.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
  * Sylius front controller.
  * Dev environment.
  */
-
+/*
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '172.33.33.1',
@@ -25,7 +25,7 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
-
+*/
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 
